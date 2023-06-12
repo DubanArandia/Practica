67fbb1915a084b6d08a232c247a44844ab1fbbbb
DubanArandia <dubanarandia@gmail.com>
2023-06-12 19:48:02 -0400

    fghh

--- Proyecto prueba 1/js/app.js
+++ Proyecto prueba 1/js/app.js
@@ -33,4 +33,28 @@ function cambioSeccion(seccion){
         behavior:'smooth'
     })
 }
+function esPrimo(numero) {
+  if (numero <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(numero); i++) {
+    if (numero % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+let contador = 0;
+let numero = 2;
+
+while (contador < 7) {
+  if (esPrimo(numero)) {
+    console.log(numero);
+    contador++;
+  }
+  numero++;
+}
 
